cf95744f70384971001bc879876975eace12efe3
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-09-14 02:50:57 +0700

    repair download button

--- app/app/koi-wave/layout.tsx
+++ app/app/koi-wave/layout.tsx
@@ -67,12 +67,12 @@ export default function RootLayout({
 						</Fragment>
 					) : null
 				}
-				{/*<Script*/}
-				{/*	async*/}
-				{/*	src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}*/}
-				{/*	strategy='lazyOnload'*/}
-				{/*	crossOrigin='anonymous'*/}
-				{/*/>*/}
+				<Script
+					async
+					src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+					strategy='lazyOnload'
+					crossOrigin='anonymous'
+				/>
 			</head>
 			<body className={`${inter.className} bg-gray text-gray-600 antialiased`}>
 				<ThemeProvider attribute={'class'}>
